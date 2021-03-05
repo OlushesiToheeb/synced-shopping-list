74a0bef6d5eec2be4217212b48c297f05137b4f4
olushesitoheeb <olaolushesi98@gmail.com>
2021-03-04 21:35:14 -0800

    building the project locally

--- public/firebase-config.js
+++ public/firebase-config.js
@@ -1,10 +1,11 @@
 importScripts('https://www.gstatic.com/firebasejs/7.23.0/firebase-app.js');
 firebase.initializeApp({
-  projectId: 'sync-shopping-list-5e6ea',
-  appId: '1:31287848266:web:9ca8c8d06c87d49e1ae5b3',
-  databaseURL: 'https://sync-shopping-list-5e6ea.firebaseio.com',
-  storageBucket: 'sync-shopping-list-5e6ea.appspot.com',
-  apiKey: 'AIzaSyAG4H_9Slix2wVifgdASaWJr_jHoeUyzow',
-  authDomain: 'sync-shopping-list-5e6ea.firebaseapp.com',
-  messagingSenderId: '31287848266',
+  apiKey: 'AIzaSyDLu0--LGT4HCbutU4tUsk1E7Ey4RfGAVk',
+  authDomain: 'sync-shopping-list-9b831.firebaseapp.com',
+  databaseURL: 'https://sync-shopping-list-9b831-default-rtdb.firebaseio.com',
+  projectId: 'sync-shopping-list-9b831',
+  storageBucket: 'sync-shopping-list-9b831.appspot.com',
+  messagingSenderId: '362292726119',
+  appId: '1:362292726119:web:50b23b7568a0a4fb6d47c2',
+  measurementId: 'G-H7TE81MBRG',
 });
